89718173453e19fc0834bae4fdf1e3104ad7a04b
Damon Reed <dreed@nomadia.org>
2023-11-14 15:33:47 -0600

    Initial files

---
+++ git-init.sh
@@ -0,0 +1,7 @@
+REMOTE = "utilities"
+
+git init
+git remote add origin https://github.com/damonreed/$REMOTE
+git add .
+git commit -m "Initial commit"
+git push origin master
---
+++ git-setup.sh
@@ -0,0 +1,6 @@
+git config --global user.email "dreed@nomadia.org"
+git config --global user.name "Damon Reed"
+git clone https://github.com/damonreed/utilities.git
+cd utilities
+git remote -v
+git branch -a
