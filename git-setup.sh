0f43cbfad3a06d162c591fee89a7bb3c926d3dc5
Damon Reed <dreed@nomadia.org>
2023-11-30 06:57:17 -0600

    Add pull.rebase false global config

--- git-setup.sh
+++ git-setup.sh
@@ -1,5 +1,6 @@
 git config --global user.email "dreed@nomadia.org"
 git config --global user.name "Damon Reed"
+git config --global pull.rebase false
 git clone https://github.com/damonreed/utilities.git
 cd utilities
 git remote -v
